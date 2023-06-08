92462977554fb34808d14aee2af4c52648df2553
carlmzamora <carlmzamora@gmail.com>
2023-06-08 20:45:04 +0800

    Added Start Menu scene
    Added start menu scene and code that allows to proceed to Main scene.
    
    Also added font.

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance;
+    
+    void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else if(Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    void Update()
+    {
+        if(SceneManager.GetActiveScene().name == SceneManager.GetSceneByBuildIndex(0).name)
+        {
+            if(Input.anyKey)
+            {
+                SceneManager.LoadScene(1);
+            }
+        }
+    }
+}
